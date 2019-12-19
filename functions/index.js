f027d131a5cc9cc1d843f386f7d87d0539b81f4c
noonedestroyer <tunagch176444@fpr.edu.vn>
2019-12-19 11:03:53 +0700

    admin function

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,24 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.userSignUp = functions.auth.user().onCreate((user)=>{
+    if (user.email && 
+        user.email == 'tunagch17644@fpt.edu.vn' &&
+        user.emailVerified) {
+            const customClaims = {
+                admin : true,
+                accessLevel: 9
+            };
+            return admin.auth().setCustomUserClaims(user.uid, customClaims)
+            .catch(error => {
+                console.log(error);
+            });
+        }
+    else
+    {
+        console.warn('not logged in');
+        console.warn(user.email);
+        console.warn(user.emailVerified);
+    }
+})
